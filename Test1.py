cb8c589f973091e838992e7c63c6c4f8481f707b
Erik Lager <65851326+warik21@users.noreply.github.com>
2023-04-08 14:19:10 +0300

    A

---
+++ Test1.py
@@ -0,0 +1,66 @@
+# Test 1
+
+# - Basic test on the balanced and unbalanced case using the algorithm with log-stabilization
+# - The KL constraints are used
+
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.utils import *
+
+n_p = 100
+n_q = 100
+n_max = 10000
+#eps = 1.e-2
+eps_vec = np.logspace(-1.,-6.,10)
+Fun = ['KL', 1.e1]
+X,Y = np.linspace(0,1,n_p), np.linspace(0,1,n_q)
+
+p =  make_1D_gauss(n_p, np.floor(3*n_p/4.), 1.)*1 + make_1D_gauss(n_p, np.floor(1*n_p/8.), 2.)*0.5
+q =  make_1D_gauss(n_q, np.floor(7*n_q/8.), 2.)*1
+
+dx = np.ones([n_p,1])/n_p
+dy = np.ones([n_q,1])/n_q
+
+C = np.zeros([n_p,n_q],dtype=np.float64)
+
+dist_f2 = lambda a,b : (a-b)**2
+dist_f1 = lambda a,b : abs(a-b)
+dist_fcos = lambda a,b : -2*np.log(np.cos(np.pi*0.5*np.minimum(1.,np.abs(a-b)/.2)))
+#dist_fcos = lambda a,b : np.minimum(1.,np.abs(a-b)/.2)
+
+for it1 in range(n_p):
+    for it2 in range(n_q):
+        C[it1,it2] = dist_f1(X[it1],Y[it2])
+
+
+R, a_t, b_t, primals, duals, pdgaps = full_scalingAlg(C,Fun,p,q,eps_vec,dx,dy,n_max)
+
+# Plots
+# Plot target and source distributions
+plt.figure( figsize=(10, 4))
+plt.plot(X,p, 'b-', label='Source distribution')
+plt.plot(Y,q, 'r-', label='Target distribution')
+plt.legend()
+plt.title('Source and target distributions')
+plt.show()
+
+# Direct output transport plan
+# Plot results
+plt.figure( figsize=(10, 4))
+plt.plot(X,p, 'b-.', label='Source distribution')
+plt.plot(Y,q, 'r-.', label='Target distribution')
+plt.plot(X, R.T@dx, 'k-', label='Final dist: R.T dx')
+plt.plot(Y, R@dy, 'g-', label='Final dist: R dy')
+plt.legend()
+plt.title('Source and target distributions')
+plt.show()
+
+# Plot transport plan with its marginals
+plt.figure( figsize=(8, 8))
+plot1D_mat(R.T@dx, R@dy, R.T, 'Transport matrix R with its marginals')
+plt.show()
+
+# Plot transport plan with its marginals
+plt.figure( figsize=(8, 8))
+plot1D_mat(p, q, R, 'Transport matrix R with the target and source dist')
+plt.show()
---
+++ utils/utils.py
@@ -0,0 +1,369 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+def div0(x, y):
+    """
+    Special x/y with convention x/0=0
+    """
+    return np.divide(x, y, out=np.zeros_like(x), where=y != 0)
+
+
+def mul0(x, y):
+    """
+    Return x*y with the convention 0*Inf = 0
+    """
+    return np.multiply(x, y, out=np.zeros_like(x), where=~np.isinf(y))
+
+
+def proxdiv(F, s, u, eps, params):
+    """
+    Proxdiv operator of the divergence function F
+
+    F: String description of the target function
+    s:
+    u:
+    eps: epsilon parameter of the entropic regularization
+    params: List of parameters of the corresponding F
+
+    F = 'KL' --> params[0] = lda ; params[1] = p
+    """
+
+    if F == 'KL':
+        lda = params[0]
+        p = params[1]
+        # return div0(p,s)**(lda/(lda+eps)) * np.exp(-u/(lda+eps))
+        return div0(p, s * np.exp(u / lda)) ** (lda / (lda + eps))
+
+    if F == 'TV':
+        lda = params[0]
+        p = params[1]
+        term1 = np.exp((- u + lda) / eps)
+        # print(u.shape)
+        # print(term1.shape)
+        # print(p.shape)
+        # print(s.shape)
+        # print((np.exp((-lda - u)/eps)).shape)
+        # print((div0(p,s)).shape)
+        term2 = np.maximum(np.exp((-lda - u) / eps), div0(p, s))
+        return np.minimum(term1, term2)
+
+    else:
+        print('Not recognized function.')
+        return
+
+
+def fdiv(F, x, p, dx, params):
+    """
+    Divergence function F
+
+    F: String description of the divergence function
+    x: Test distribution (KL_F(x|p))
+    p: Reference distribution (KL_F(x|p))
+    dx: discretization vector
+    params: List of parameters of the corresponding F
+
+    F = 'KL' --> params[0] = lda
+    """
+
+    if F == 'KL':
+        lda = params[0]
+        return lda * np.sum(mul0(dx, (mul0(x, np.log(div0(x, p))) - x + p)))
+
+    elif F == 'TV':
+        lda = params[0]
+        return lda * np.sum(mul0(dx, abs(x - p)))
+
+    else:
+        print('Not recognized function.')
+        return
+
+
+def fdiv_c(F, x, p, dx, params):
+    """
+    Convex conjugate of the divergence function F
+
+    F: String description of the divergence function
+    x: Test distribution (KL_F(x|p))
+    p: Reference distribution (KL_F(x|p))
+    dx: discretization vector
+    params: List of parameters of the corresponding F
+
+    F = 'KL' --> params[0] = lda
+    """
+
+    if F == 'KL':
+        lda = params[0]
+        return lda * np.sum(mul0(p * dx, np.exp(x / lda) - 1))
+
+    elif F == 'TV':
+        lda = params[0]
+        return lda * np.sum(mul0(dx, np.minimum(p, np.maximum(-p, mul0(p, x / lda)))))
+
+    else:
+        print('Not recognized function.')
+        return
+
+
+def simple_scalingAlg(C, Fun, p, q, eps, dx, dy, n_max):
+    """
+    Simple implementation for solving Unbalanced OT problems
+
+    C: Cost matrix
+    Fun: List defining the function and its lambda parameter. e.i. Fun = ['KL', 0.01]
+    p: Source distribution
+    q: target dstribution
+    eps: epsilon parameter
+    dx: discretizaiton vector in x / np.shape(dx) = (nJ,1)
+    dy: discretization vector in y / np.shape(dy) = (nJ,1)
+    n_max: Max number of iterations
+    """
+
+    # Init
+    nI = C.shape[0]
+    nJ = C.shape[1]
+    a_t = np.ones([nI, 1])
+    b_t = np.ones([nJ, 1])
+    pdgap = np.zeros([n_max, 1])
+    F = Fun[0]
+    lda = Fun[1]  # define lambda parameter value
+
+    K_t = np.exp(C / (-eps))
+
+    # Main Loop
+    for it in range(n_max):  # -> Use for and cutting condition
+        params = [lda, p]
+        a_t = proxdiv(F, (K_t @ (b_t * dy)), 0., eps, params)
+
+        params = [lda, q]
+        b_t = proxdiv(F, (K_t.T @ (a_t * dx)), 0., eps, params)
+
+        # Gap calculation
+        R = (np.tile(a_t, nJ) * K_t) * np.tile(b_t, nI).T
+        param_p = [lda]
+        primal = fdiv(F, np.dot(R, dy), p, dx, param_p) + fdiv(F, np.dot(R.T, dx), q, dy, param_p) + \
+                 eps / (nI * nJ) * np.sum(mul0(R, np.log(div0(R, K_t))) - R + K_t)
+        dual = - fdiv_c(F, -eps * np.log(a_t), p, dx, param_p) - fdiv_c(F, -eps * np.log(b_t), q, dy, param_p) - \
+               eps / (nI * nJ) * np.sum(R - K_t)
+        pdgap[it] = primal - dual
+
+    R = (np.tile(a_t, nJ) * K_t) * np.tile(b_t, nI).T
+
+    return R, pdgap, a_t, b_t
+
+
+def make_1D_gauss(n, m, s):
+    """
+    Return a 1D histogram for a gaussian distribution (n bins, mean m and std s)
+    Parameters
+    ----------
+    n : int
+        number of bins in the histogram
+    m : float
+        mean value of the gaussian distribution
+    s : float
+        standard deviaton of the gaussian distribution
+    Returns
+    -------
+    h : np.array (n,)
+          1D histogram for a gaussian distribution
+    """
+    x = np.arange(n, dtype=np.float64)
+    h = np.exp(-(x - m) ** 2 / (2 * s ** 2))
+    h = h / h.sum()
+    return np.reshape(h, (len(h), 1))
+
+
+def plot1D_mat(a, b, M, title=''):
+    """ Plot matrix M  with the source and target 1D distribution
+
+    Creates a subplot with the source distribution a on the left and
+    target distribution b on the tot. The matrix M is shown in between.
+
+
+    Parameters
+    ----------
+    a : np.array, shape (na,)
+        Source distribution
+    b : np.array, shape (nb,)
+        Target distribution
+    M : np.array, shape (na,nb)
+        Matrix to plot
+    title: string, optional (default='')
+    """
+    na, nb = M.shape
+
+    gs = gridspec.GridSpec(3, 3)
+
+    xa = np.arange(na)
+    xb = np.arange(nb)
+
+    ax1 = plt.subplot(gs[0, 1:])
+    plt.plot(xb, b, 'r', label='Target distribution')
+    plt.yticks(())
+    plt.title(title)
+
+    ax2 = plt.subplot(gs[1:, 0])
+    plt.plot(a, xa, 'b', label='Source distribution')
+    plt.gca().invert_xaxis()
+    plt.gca().invert_yaxis()
+    plt.xticks(())
+
+    plt.subplot(gs[1:, 1:], sharex=ax1, sharey=ax2)
+    plt.imshow(M, interpolation='nearest')
+    plt.axis('off')
+
+    plt.xlim((0, nb))
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0., hspace=0.2)
+
+
+def plot2D_samples_mat(xs, xt, G, thr=1e-8, **kwargs):
+    """ Plot matrix M  in 2D with  lines using alpha values
+
+    Plot lines between source and target 2D samples with a color
+    proportional to the value of the matrix G between samples.
+
+
+    Parameters
+    ----------
+    xs : ndarray, shape (ns,2)
+        Source samples positions
+    xt : ndarray, shape (nt,2)
+        Target samples positions
+    G : ndarray, shape (na,nb)
+        OT matrix
+    thr : float, optional
+        threshold above which the line is drawn
+    **kwargs : dict
+        paameters given to the plot functions (default color is black if
+        nothing given)
+    """
+    if ('color' not in kwargs) and ('c' not in kwargs):
+        kwargs['color'] = 'k'
+    mx = G.max()
+    for i in range(xs.shape[0]):
+        for j in range(xt.shape[0]):
+            if G[i, j] / mx > thr:
+                plt.plot([xs[i, 0], xt[j, 0]], [xs[i, 1], xt[j, 1]],
+                         alpha=G[i, j] / mx, **kwargs)
+
+
+def full_scalingAlg(C, Fun, p, q, eps_vec, dx, dy, n_max, verb=False, eval_rate=10):
+    """
+    Implementation for solving Unbalanced OT problems that includes the log-domain stabilization
+
+    C: Cost matrix
+    Fun: List defining the function and its lambda parameter. e.i. Fun = ['KL', 0.01]
+    p: Source distribution
+    q: target dstribution
+    eps_vec: epsilon parameter (If scalar, the same epsilons is used throughout the algorithm.
+        If it is a vector, the epsilons are equally distributed along the iterations forcing an absorption
+        at each epsilon change.)
+    dx: discretizaiton vector in x / np.shape(dx) = (nJ,1)
+    dy: discretization vector in y / np.shape(dy) = (nJ,1)
+    n_max: Max number of iterations
+    """
+
+    # Initialization
+    nI = C.shape[0]
+    nJ = C.shape[1]
+    a_t = np.ones([nI, 1])
+    u_t = np.zeros([nI, 1])
+    b_t = np.ones([nJ, 1])
+    v_t = np.zeros([nJ, 1])
+    F = Fun[0]
+    lda = Fun[1]  # define lambda parameter value
+    eps_ind = 0  # index for the chosen epsilons
+    eval_rate = eval_rate
+    n_evals = np.floor(n_max / eval_rate).astype(int)
+    param_p = [lda]
+    primals = np.zeros((n_evals, 1))
+    duals = np.zeros((n_evals, 1))
+    pdgaps = np.zeros((n_evals, 1))
+
+    if np.isscalar(eps_vec):
+        eps = eps_vec
+        eps_tot = 1
+    else:
+        eps = eps_vec[eps_ind]  # select the first epsilon to use
+        eps_tot = len(eps_vec)
+
+    K_t = np.exp(C / (-eps))
+
+    # Main Loop
+    for it in range(n_max):  # -> Use for and cutting condition
+        params = [lda, p]
+        a_t = proxdiv(F, (K_t @ (b_t * dy)), u_t, eps, params)
+
+        params = [lda, q]
+        b_t = proxdiv(F, (K_t.T @ (a_t * dx)), v_t, eps, params)
+
+        if verb:
+            # Check the primal, dual and primal-dual gap
+            if it % eval_rate == 0:
+                it_eval = 0
+                R = (np.tile(a_t, nJ) * K_t) * np.tile(b_t, nI).T  # Reconstruct map
+                primal = fdiv(F, R @ dy, p, dx, param_p) + fdiv(F, R.T @ dx, q, dy, param_p) + \
+                         eps / (nI * nJ) * np.sum(mul0(R, np.log(div0(R, K_t))) - R + K_t)
+                dual = - fdiv_c(F, -eps * np.log(a_t), p, dx, param_p) - fdiv_c(F, -eps * np.log(b_t), q, dy, param_p) - \
+                       eps / (nI * nJ) * np.sum(R - K_t)
+                pdgap = primal - dual
+
+                # print("primal = %f \n dual = %f \n pdgap = %f \n"%(primal,dual,pdgap))
+
+                primals[it_eval] = primal
+                duals[it_eval] = dual
+                pdgaps[it_eval] = pdgap
+
+                it_eval += 1
+
+        # stabilizations
+        # print('it/n_max = %f , (eps_ind+1)/len(eps_vec) = %f'%(it/n_max , (eps_ind+1)/len(eps_vec)))
+        if np.max([abs(a_t), abs(b_t)]) > 1e50 or (it / n_max) > (eps_ind + 1) / eps_tot:  # or it == n_max-1:
+            """
+            primal = fdiv(F,R@dy,p,dx,param_p) + fdiv(F,R.T@dx,q,dy,param_p) + \
+                eps/(nI*nJ) * np.sum( mul0(R , np.log(div0(R,K_t))) - R + K_t )
+            dual = - fdiv_c(F,-eps*np.log(a_t),p,dx,param_p) - fdiv_c(F,-eps*np.log(b_t),q,dy,param_p) -\
+                eps/(nI*nJ) * np.sum(R-K_t)
+            pdgap = primal-dual
+            """
+
+            # absorb
+            u_t = u_t + eps * np.log(a_t)
+            v_t = v_t + eps * np.log(b_t)
+
+            if (it / n_max) > (eps_ind + 1) / eps_tot:
+                eps_ind += 1
+                eps = eps_vec[eps_ind]
+
+            # update K
+            K_t = np.exp((np.tile(u_t, nJ) + np.tile(v_t, nI).T - C) / eps)
+
+            a_t = np.ones([nI, 1])  # Not really needed
+            b_t = np.ones([nJ, 1])
+            print('it = %d , eps = %f' % (it, eps))
+
+    R = (np.tile(a_t, nJ) * K_t) * np.tile(b_t, nI).T  #  Reconstruct map
+
+    return R, a_t, b_t, primals, duals, pdgaps
+
+
+def signed_GWD(C, Fun, p, q, eps_vec, dx, dy, n_max, verb=True, eval_rate=10):
+    p_pos = np.zeros(p.shape)
+    p_neg = np.zeros(p.shape)
+    q_pos = np.zeros(q.shape)
+    q_neg = np.zeros(q.shape)
+
+    sign_p = np.sign(p)
+    sign_q = np.sign(q)
+
+    p_pos[sign_p > 0] = p[sign_p > 0]
+    p_neg[sign_p < 0] = -p[sign_p < 0]
+    q_pos[sign_q > 0] = q[sign_q > 0]
+    q_neg[sign_q < 0] = -q[sign_q < 0]
+
+    p_tilde = p_pos + q_neg
+    q_tilde = q_pos + p_neg
+
+    return full_scalingAlg(C, Fun, p_tilde, q_tilde, eps_vec, dx, dy, n_max, verb, eval_rate)
