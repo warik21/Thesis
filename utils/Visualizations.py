e1a0acd073887c53d60be7efc2689ad8e7aeea7d
Erik Lager <65851326+warik21@users.noreply.github.com>
2023-07-25 21:42:41 +0300

    Maybe its not the answer which is flawed, but your logic - A.Moscovich

--- utils/Visualizations.py
+++ utils/Visualizations.py
@@ -19,25 +19,18 @@ def plot_distribution(X, p, q, title):
     plt.legend()
     plt.show()
 
-def plot_marginals(X, p, q, Transport_plan, title):
+def plot_marginals(X, p, q, title):
     """
     Plot the marginals of the transport map
     :param X: linear space in which the distributions are defined
     :param p: source distribution
     :param q: target distribution
-    :param Transport_plan: transport plan
     :param title: title of the plot
     :return: None, plot the distributions
     """
     plt.figure(figsize=(10, 4))
-    n_p = len(p)
-    n_q = len(q)
-    dx = np.ones(n_p) / n_p
-    dy = np.ones(n_q) / n_q
     plt.plot(X, p, 'b-.', label='Source dist: p')
     plt.plot(X, q, 'r-.', label='Target dist: q')
-    plt.plot(X, Transport_plan.T @ dx, 'k-', label='Final source dist (q): Transport_plan.T dx')
-    plt.plot(X, Transport_plan @ dy, 'g-', label='Final target dist (p): Transport_plan dy')
     plt.title(title)
     plt.legend()
     plt.show()
@@ -174,3 +167,47 @@ def plot2D_samples_mat(xs, xt, G, thr=1e-8, **kwargs):
                 plt.plot([xs[i, 0], xt[j, 0]], [xs[i, 1], xt[j, 1]],
                          alpha=G[i, j] / mx, **kwargs)
 
+
+def plot1D_mat_bar(a, b, M, title=''):
+    """ Plot matrix M  with the source and target 1D distribution
+
+    Creates a subplot with the source distribution a on the left and
+    target distribution b on the tot. The matrix M is shown in between.
+
+
+    Parameters
+    ----------
+    a : np.array, shape (na,)
+        Source distribution
+    b : np.array, shape (nb,)
+        Target distribution
+    M : np.array, shape (na,nb)
+        Matrix to plot
+    title: string, optional (default='')
+    """
+    na, nb = M.shape
+
+    gs = gridspec.GridSpec(3, 3)
+
+    xa = np.arange(na)
+    xb = np.arange(nb)
+
+    ax1 = plt.subplot(gs[0, 1:])
+    plt.bar(xb, b, label='Target distribution')
+    plt.yticks(())
+    plt.title(title)
+
+    #because of barh syntax, a and xa should be in reverse order.
+    ax2 = plt.subplot(gs[1:, 0])
+    plt.barh(xa, a, label='Source distribution')
+    plt.gca().invert_xaxis()
+    plt.gca().invert_yaxis()
+    plt.xticks(())
+
+    plt.subplot(gs[1:, 1:], sharex=ax1, sharey=ax2)
+    plt.imshow(M, interpolation='nearest')
+    plt.axis('off')
+
+    plt.xlim((0, nb))
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0., hspace=0.2)
